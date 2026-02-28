7cee85d14065f39aa29b9d3aedf02defa6866759
vivek channe <vivekchanne06@gmail.com>
2026-02-28 23:08:42 +0530

    update api

--- Frontend/src/features/auth/services/post.api.js
+++ Frontend/src/features/auth/services/post.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/post",
+  baseURL: "https://insta-clone-va4x.onrender.com/api/post",
   withCredentials: true,
 });
 
--- Frontend/src/features/auth/services/user.api.js
+++ Frontend/src/features/auth/services/user.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/user",
+  baseURL: "https://insta-clone-va4x.onrender.com/api/user",
   withCredentials: true,
 });
 
